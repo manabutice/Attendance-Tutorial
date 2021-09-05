1831a9a99bc8af2cc2ddf1c0a65e94cbaf4c3296
EC2 Default User <ec2-user@ip-172-31-5-193.ap-northeast-1.compute.internal>
2021-09-05 03:19:37 +0000

    Change from HTTPS to SSH

--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20210328005013) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.boolean "admin", default: false 
+    t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-06-26 23:00:00"
-    t.datetime "work_time", default: "2021-06-26 22:30:00"
+    t.datetime "basic_time", default: "2021-09-04 22:00:00"
+    t.datetime "work_time", default: "2021-09-04 23:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
